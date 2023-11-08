9f5f57bd9898d9fa2cdf94df78fbb71d1d564cb4
Zhengyi Jiang <anthony.jiang.github@outlook.com>
2023-11-08 22:15:26 +0000

    fixed spelling mistakes

--- install/uninstall.sh
+++ install/uninstall.sh
@@ -2,8 +2,9 @@ export SUDO_ASKPASS=${HOME}/Unitree/autostart/passwd.sh
 
 # Revert autostart files
 autostart_dir=${HOME}/.config/autostart
+rm ${autostart_dir}/ice9_autostart.sh.desktop
 if [ -f ${autostart_dir}/update.sh.desktop.bak ]; then
-    mv ${autostart_dir}/ice9_autostart.sh.desktop.bak ${autostart_dir}/ice9_autostart.sh.desktop
+    mv ${autostart_dir}/update.sh.desktop.bak ${autostart_dir}/update.sh.desktop
 fi
 if [ -f ${autostart_dir}/slam_planner.sh.desktop.bak ]; then
     mv ${autostart_dir}/slam_planner.sh.desktop.bak ${autostart_dir}/slam_planner.sh.desktop
